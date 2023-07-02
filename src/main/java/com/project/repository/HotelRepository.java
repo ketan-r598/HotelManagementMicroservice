9edc5888fa8bb38b60e756c8d685432295939ded
ketan_r <ketan_r598@outlook.com>
2023-07-02 12:03:57 +0530

    Added maiden method

--- src/main/java/com/project/repository/HotelRepository.java
+++ src/main/java/com/project/repository/HotelRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.project.model.Hotel;
+import java.util.List;
+
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Integer> {
 
+	List<Hotel> findByUserId(int userId);
 }
